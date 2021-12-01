33feb6339bb74b9937c6b0bc1a8569232f17404d
Calum McDonald <calumcdonald@gmail.com>
2021-12-01 14:07:51 +0000

    Day1 part 1

--- src/day1.ts
+++ src/day1.ts
@@ -4,16 +4,31 @@ import readline from "readline";
 const day1 = async (): Promise<number> => {
     const fileStream = fs.createReadStream('src/input/day1.txt');
 
+    let lineCount = 0;
     const rl = readline.createInterface({
         input: fileStream,
         crlfDelay: Infinity
     });
+    
+    let increasedCount = 0;
+    let lastDepth: number = 0;
+    let currentDepth: number = 0;
 
     for await(const line of rl){
-        console.log(line);
+        if(lineCount === 0) lastDepth = Number(line); 
+        
+        currentDepth = Number(line);
+
+        if(currentDepth > lastDepth){
+            increasedCount++;
+        }
+
+        lastDepth = currentDepth;
+
+        lineCount++;
     }
 
-    return 0;
+    return increasedCount;
 }
 
 export default day1;
