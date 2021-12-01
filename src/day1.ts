847de0c9fe4c9bf7ffa6eb79e2a0a34cb57ecd76
Calum McDonald <calumcdonald@gmail.com>
2021-12-01 21:03:44 +0000

    Refactor

--- src/day1.ts
+++ src/day1.ts
@@ -1,34 +1,13 @@
-import fs from "fs";
-import readline from "readline";
+import getLines from "./getLines.js";
 
 const day1 = async (): Promise<number> => {
-    const fileStream = fs.createReadStream('src/input/day1.txt');
-
-    let lineCount = 0;
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-    });
-    
-    let increasedCount = 0;
-    let lastDepth: number = 0;
-    let currentDepth: number = 0;
-
-    for await(const line of rl){
-        if(lineCount === 0) lastDepth = Number(line); 
-        
-        currentDepth = Number(line);
-
-        if(currentDepth > lastDepth){
-            increasedCount++;
+    return getLines('src/input/day1.txt').then((lines) => {
+        let count = 0;
+        for(let i = 1; i < lines.length; i++){
+            if(lines[i] > lines[i-1]) count++;
         }
-
-        lastDepth = currentDepth;
-
-        lineCount++;
-    }
-
-    return increasedCount;
+        return count;
+    });
 }
 
 export default day1;
---
+++ src/getLines.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+import readline from "readline";
+
+const getLines = async (path: string): Promise<string[]> => {
+    const fileStream = fs.createReadStream('src/input/day1.txt');
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    const lines: string[] = [];
+
+    for await(const line of rl){
+        lines.push(line);
+    }
+
+    return lines;
+}
+
+export default getLines;
